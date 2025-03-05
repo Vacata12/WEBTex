bb41dc0bee141435c883a355f14c3dcd625f26cc
Ivan Stoynev <79401987+Vacata12@users.noreply.github.com>
2025-03-05 11:41:45 +0200

    add delete

--- Ivan/todo-list/index.js
+++ Ivan/todo-list/index.js
@@ -1,8 +1,15 @@
-function onClickAdd()
-{
+function onClickAdd() {
     const input = document.querySelector('input');
-    const ul = document.querySelector('ul')
-    const li = document.createElement('li')
-    li.textContent = input.value
-    ul.appendChild(li)
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    li.onclick = function() {
+        onClickDelete(li);
+    };
+    ul.appendChild(li);
+}
+
+function onClickDelete(li) {
+    const ul = document.querySelector('ul');
+    ul.removeChild(li);
 }
