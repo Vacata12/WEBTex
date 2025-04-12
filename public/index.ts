1abb8d01469c8fb8d74682964fe48bd339f13be5
Ivan Stoynev <79401987+Vacata12@users.noreply.github.com>
2025-04-12 19:19:58 +0300

    Change to typescript

---
+++ public/index.ts
@@ -0,0 +1,19 @@
+const uploadFile = (file: File): void => {
+    const formData = new FormData();
+    formData.append("uploadFile", file); // Attach the file to the request
+
+    fetch("http://localhost:3000/api/uploadfile", {
+        method: "POST",
+        body: formData, // Send the FormData object
+    })
+        .then((response) => response.text()) // Handle the server response
+        .then((data) => console.log(data)) // Log the response
+        .catch((error) => console.error("Error:", error)); // Handle errors
+};
+
+const downloadFile = (): void => {
+    fetch("http://localhost:3000/api/download")
+        .then((res) => res.text())
+        .then((data) => console.log(data)) // Log the response
+        .catch((error) => console.error("Error:", error)); // Handle errors
+};
