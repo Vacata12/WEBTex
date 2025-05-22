7ba93c13be07da78f90d9e91e81fc2943648a345
Tarator47 <atiswag47@gmail.com>
2025-05-22 23:48:35 +0300

    Add database seeding and range query examples for MongoDB

---
+++ Polly/seed.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require('mongodb');
+const fs = require('fs');
+
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri);
+const dbName = 'demoDB';
+
+async function seedDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+
+    const db = client.db(dbName);
+    const collection = db.collection('usersPolly');
+
+    // Изчистване на съществуващите данни
+    await collection.deleteMany({});
+
+    // Зареждане на примерни данни
+    const data = JSON.parse(fs.readFileSync('mockData.json', 'utf8'));
+    await collection.insertMany(data);
+
+    console.log('Базата данни е успешно заредена');
+  } catch (err) {
+    console.error('Грешка при зареждане на базата данни:', err);
+  } finally {
+    await client.close();
+  }
+}
+
+seedDatabase();
