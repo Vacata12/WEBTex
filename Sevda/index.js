1980b1e0a551a595e8b3a86a98b30d1d143d1409
Tarator47 <atiswag47@gmail.com>
2025-05-23 00:45:01 +0300

    Enable database seeding and cursor iteration example in index.js

--- Sevda/index.js
+++ Sevda/index.js
@@ -11,23 +11,22 @@ async function main() {
         const db = client.db("newdb"); // нова база данни
         const clients = db.collection("clients"); // нова колекция
  
- 
-        // await clients.insertMany([
-        //     { firstName: "Ivan", lastName: "Ivanov", age: 28, country: "Bulgaria" },
-        //     { firstName: "Maria", lastName: "Petrova", age: 22, country: "Bulgaria" },
-        //     { firstName: "Georgi", lastName: "Dimitrov", age: 35, country: "Bulgaria" },
-        //     { firstName: "Dimitar", lastName: "Stoyanov", age: 31, country: "Bulgaria" },
-        //     { firstName: "Nikolay", lastName: "Ivanov", age: 33, country: "Bulgaria" }
-        // ]);
+        await clients.insertMany([
+            { firstName: "Ivan", lastName: "Ivanov", age: 28, country: "Bulgaria" },
+            { firstName: "Maria", lastName: "Petrova", age: 22, country: "Bulgaria" },
+            { firstName: "Georgi", lastName: "Dimitrov", age: 35, country: "Bulgaria" },
+            { firstName: "Dimitar", lastName: "Stoyanov", age: 31, country: "Bulgaria" },
+            { firstName: "Nikolay", lastName: "Ivanov", age: 33, country: "Bulgaria" }
+        ]);
  
         console.log("======================");
-        //Пример 1 - итерация с курсор, документи с age >= 30
-        // const cursor = clients.find({ age: { $gte: 30 } }).batchSize(2);
-        // while (await cursor.hasNext()) {
-        //     const user = await cursor.next();
-        //     console.log(`Processing: ${user.firstName} ${user.lastName}, Age: ${user.age}`);
-        //     console.log('*******');
-        // }
+        // Пример 1 - итерация с курсор, документи с age >= 30
+        const cursor = clients.find({ age: { $gte: 30 } }).batchSize(2);
+        while (await cursor.hasNext()) {
+            const user = await cursor.next();
+            console.log(`Processing: ${user.firstName} ${user.lastName}, Age: ${user.age}`);
+            console.log('*******');
+        }
  
         console.log("======================");
  
